1ad8ee35c57bed416b29f0a8afa5a12a0f9c24c7
J King Chang <105471690+JKing2000@users.noreply.github.com>
2025-11-19 23:05:41 +1100

    Create 1_prepare_download_for_offline.sh

---
+++ 1_prepare_download_for_offline.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PBS -N download
+#PBS -q copyq
+#PBS -l ncpus=1,walltime=10:00:00,storage=gdata/if89+gdata/xl04,mem=20GB
+#PBS -j oe
+#PBS -M z5205618@ad.unsw.edu.au
+#PBS -m ae
+
+module use /g/data/if89/apps/modulefiles
+module load egapx/0.4.1-alpha
+module load nextflow/25.04.6
+module load sratoolkit/3.1.1
+
+egapx.py -e nci-gadi \
+--force -dl \
+-lc /path/to/store/download/files \
+/path/to/input.yaml
+
+#If the job is killed due to walltime exceeding limit, resubmit again and it'll continue downloading from where it left off.
